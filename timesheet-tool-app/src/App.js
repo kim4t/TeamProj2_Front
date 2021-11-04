379ec0b3b56e3016e24a06823fd5d06ff610135a
Axel Geoffredo <axel.geoffredo15@gmail.com>
2021-11-04 14:44:34 -0400

    added login and calendar 7 days function

--- timesheet-tool-app/src/App.js
+++ timesheet-tool-app/src/App.js
@@ -2,29 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import TimeSheet from './components/TimeSheet';
 import TimeSheetTable from './components/TimeSheetTable';
+import Login from './components/login';
 
 function App() {
   return (
-    <div>
-      <TimeSheet/>
-      <TimeSheetTable/>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    // <TimeSheet/>
+    <Login/>
   );
 }
 
